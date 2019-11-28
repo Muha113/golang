3d9dcae7266fd90114d9a8677b483b3e95449f85
Muha113 <43477286+Muha113@users.noreply.github.com>
2019-11-28 19:03:17 +0300

    edited regex and math.Max*

--- task/task.go
+++ task/task.go
@@ -2,26 +2,19 @@ package task
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
-	"strings"
 )
-
-// Max values depending on system bit depth
-var maxUint = ^uint(0)
-var minUint = 0
-var maxInt = int(maxUint >> 1)
-var minInt = -maxInt - 1
 
 // MyStrToInt1 : conversion string to int 1-t way
 func MyStrToInt1(str string) (int, error) {
-	str = strings.TrimSpace(str)
 	resultInt, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		return 0, fmt.Errorf("Error occured: %s", err.Error())
 	}
 	offset := resultInt - float64(int(resultInt))
-	if resultInt > float64(maxInt) || resultInt < float64(minInt) || offset != 0.0 {
+	if resultInt > float64(math.MaxInt64) || resultInt < float64(math.MinInt64) || offset != 0.0 {
 		return 0, fmt.Errorf("Error occured: %s", "str do not match int template")
 	}
 	return int(resultInt), nil
@@ -29,20 +22,19 @@ func MyStrToInt1(str string) (int, error) {
 
 // MyStrToInt2 : conversion string to int 2-d way
 func MyStrToInt2(str string) (int, error) {
-	regexMatch := "^\\s*[+-]{0,1}[0-9]+(.[0]+|)\\s*$"
-	regexSearch := regexp.MustCompile("[1-9]+[0-9]*")
-	matched, _ := regexp.MatchString(regexMatch, str)
+	regexMatch := regexp.MustCompile("^[+-]?(\\d+)(.[0]+|)$")
+	matched := regexMatch.MatchString(str)
 	if !matched {
 		return 0, fmt.Errorf("Error occured: %s", "str do not match int template")
 	}
-	myStr := regexSearch.FindString(str)
+	myStr := regexMatch.FindStringSubmatch(str)
 	var coeff int
 	if str[0] == '-' {
 		coeff = -1
 	} else {
 		coeff = 1
 	}
-	resultInt, err := strconv.Atoi(myStr)
+	resultInt, err := strconv.Atoi(myStr[1])
 	if err != nil {
 		return 0, fmt.Errorf("Error occured: %s", err.Error())
 	}
